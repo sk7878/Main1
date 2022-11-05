bada3efac0ff5d06673cbd117375995d8330f9d7
Sujith Kumaar <sujith.kumaar@outlook.com>
2022-11-04 23:39:26 -0700

    cook 28

--- topviewed.php
+++ topviewed.php
@@ -25,6 +25,52 @@
             </div>
 
 
+            </div>
+        	<div class="col-lg-7 col-sm-8 wow fadeInRight">
+            	<h2>Your top viewed</h2>
+                <?php
+                    if(isset($_COOKIE["lastids"])){
+                        echo "<table>";
+                        echo "<tr>";
+                        echo "<th>Name</th>";
+                        echo "<th>Hits</th>";
+                        echo "</tr>";        
+                     $heatmap=array();
+                     foreach (explode(",",$_COOKIE["lastids"]) as $id){
+                         if(isset($heatmap[$id])){
+                             $heatmap[$id] = $heatmap[$id] + 1;
+                         }
+                         else {
+                             $heatmap[$id] = 1;
+                         }
+                     }
+                    for($i=0;$i<5;$i++){
+                     $max=0;
+                     $maxid=0;
+                    foreach ($heatmap as $key => $value){
+                         if($value>$max){
+                             $max = $value;
+                             $maxid = $key;
+                         }
+                     }
+                    $result = $conn->query("SELECT * FROM services where id = ".$maxid.";");
+                    $row = $result->fetch_assoc();
+                    echo "<tr>";
+                    echo "<td>".$row["name"]."</td>";
+                    echo "<td>".$row["description"]."</td>";
+                    echo "</tr>";
+                    unset($heatmap[$maxid]);    
+                    }
+                     
+                    }
+                    else{
+                        echo "You have not viewed any services";
+                          
+                    }
+                    echo "</table>";
+                ?>
+
+
 <h2>Your Last 5 viewed</h2>
 
                 
