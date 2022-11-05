b508e78fd722a01c9a51a88adc535e647dbf443a
Sujith Kumaar <sujith.kumaar@outlook.com>
2022-11-04 23:06:41 -0700

    cook 20

--- services/architecture.php
+++ services/architecture.php
@@ -11,15 +11,18 @@
     $conn->close();
 ?>
 <?php
+echo $prod["name"].",1";  
     if(isset($_COOKIE["lastids"])){
+		echo $prod["name"].",2";  
         if(explode(",",$_COOKIE["lastids"])[0]!=$prod["id"]){
-            setcookie("lastids", 1.",".$_COOKIE["lastids"],time() + (86400 * 30)); 
+            setcookie("lastids", '1'.",".$_COOKIE["lastids"],time() + (86400 * 30)); 
 			echo "cookie update";   
         }
         
     }
     else{
-        setcookie("lastids", 1, time() + (86400 * 30));
+		echo $prod["name"].",3"; 
+        setcookie("lastids", '1', time() + (86400 * 30));
 		echo "cookie set";
     }
 ?>
